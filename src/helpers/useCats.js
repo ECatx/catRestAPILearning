503ccd36908d7f639da098ea2f4bf701a28a4198
Eric Castellanos <eric@ericcastellanos.com>
2021-03-28 08:54:17 -0500

    Updates to page

--- src/helpers/useCats.js
+++ src/helpers/useCats.js
@@ -11,14 +11,12 @@ export const searchItem = ref('')
       api_key: "1f913fd2-a737-4934-9004-b86bbe545911",
     },
   })
-  
   export const getCats = async () => {
       try {
           const ressource = searchItem.value ? 'images/search' : 'images/search' 
           const response = await api(ressource,{ params: {limit:16, size:"full"} })
           cats.value = response.data
           // console.log(response.data[0].url)
-
       } catch (error) {
           console.log(error)
       }
--- src/pages/Info.vue
+++ src/pages/Info.vue
@@ -2,7 +2,10 @@
     <div class="mt-6 grid justify-items-center space-y-5">
         <h1 class="text-white text-2xl font-bold">Info Page of image:  {{id}}</h1>
                 <div v-if="cat">
-            <img :src=cat.url>
+                <div v-if="cat.categories">
+                <h1 class="text-white text-2xl text-center font-bold">Category:  {{cat.categories[0].name}}</h1>
+            </div>
+            <img :src=cat.url>         
         </div> 
     </div>
 </template>
@@ -16,16 +19,11 @@ export default {
             cat: null
         }
     },
-    // watch:{
-    //     '$route' (to,from){
-    //         this.getData()
-    //     }
-    // },
     methods: {
         getData(){
             this.id
             var myVue = this
-            axios.get('https://api.thecatapi.com/v1/images/'+this.id)
+            axios.get('https://api.thecatapi.com/v1/images/'+this.id,{ params: {size:"full"} })
             .then(function (response){
                 myVue.cat = response.data
                 console.log(response)
